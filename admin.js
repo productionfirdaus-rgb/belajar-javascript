975a6558cd1712fc8c633741e426826fd6583aa4
productionfirdaus-rgb <productionfirdaus@gmail.com>
2026-06-29 13:36:55 +0700

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,4 +1,4 @@
-const API_URL = 'hhttps://script.google.com/macros/s/AKfycbyRRo-Bsi0k3bQw2NmDX-W1VHYHViSYJ_XshqEtBDxj5lSgW1AyqocoZbV_70eJYA6v/exec';
+const API_URL = 'https://script.googleusercontent.com/macros/echo?user_content_key=AUkAhnSJ0bV8UkRu66ZbQCdLhUQGR6UQtsalqVEWzyNBrsh8D3kXUIiWKFKzdi1yg6r6v1FfTyb4LUJyphXbqipkoVAj2Wd5X_UA6S0aftyvlaYzxUv-jp2oS-WH-RV86aNv3YSyTQk4tU6uyxHjKESWYycJ4P6C8isTIxUbPSaQZDqVD9jjn3llKTZCS3eBa8jYyEwFPZ04NY_U8R160e7H_KZsDIwNaZo3FWnghAP0Yhm-Uz72pfhdq8nNN7SgjV13iaso1KLqzrTXPcAutrrISHVZgevv5A&lib=MK5-Q8VkVkeL_sCr8Q5MYTQ3tbNh787q5';
 
 async function loadData() {
     const res = await fetch(`${API_URL}?action=ambilDataAdmin`);
